40cd2e81aab73ea425011367102cbdde1ac1adc9
Andre Rosot <alrosot@yahoo.com.br>
2017-11-21 07:05:07 +1100

    Saving or updating peer

--- src/main/kotlin/br/com/trofo/seeder/PeerService.java
+++ src/main/kotlin/br/com/trofo/seeder/PeerService.java
@@ -1,5 +1,6 @@
 package br.com.trofo.seeder;
 
+import br.com.trofo.seeder.dao.PeerDao;
 import br.com.trofo.seeder.dao.PeerRepository;
 import br.com.trofo.seeder.entity.Peer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,18 @@ public class PeerService {
     @Autowired
     private PeerRepository peerRepository;
 
+    @Autowired
+    private PeerDao peerDao;
+
     public Peer registerPeer(String infohash, String ip, int port) {
         Peer peer = new Peer();
         peer.setInfoHash(infohash);
         peer.setIp(ip);
         peer.setPort(port);
         peer.setExpires(new Date()); //TODO calculate
+
+        peerDao.persistEntity(peer);
+
         return peerRepository.save(peer);
     }
 }
